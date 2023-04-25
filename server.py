800e488668e970cc30fd6aead903f864eaef157c
bud-caolibin <caolibin@budcreate.io>
2023-04-25 16:38:05 +0800

    feat:调整server代码

--- server.py
+++ server.py
@@ -1,31 +1,40 @@
 import asyncio
 from connection import Connection
 from logger import logger
+from typing import Tuple
 
 
-class Tcp(object):
+class TcpServer(object):
 
-    def __init__(self, port: int, name: str):
+    def __init__(self, port: int,):
         self._port: int = port
-        self._name: str = name
 
     async def run_server(self):
-        async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-            connection = Connection(reader, writer)
-            remote_connection = await self.connect("xxx", 0000)
-            logger.info(f"Client Connected.")
+        async def handle_client(local_reader: asyncio.StreamReader, local_writer: asyncio.StreamWriter):
+            remote_reader, remote_writer = await self.connect("xxx", 0000)
+            connection_local = Connection(local_reader, remote_writer)
+            connection_remote = Connection(remote_reader, local_writer)
             await asyncio.gather(
-                    connection.handle_message(),
+                    connection_local.handle_message(),
+                    connection_remote.handle_message(),
                     )
 
         server = await asyncio.start_server(handle_client, port=self._port)
-        logger.info(f"{self._name} Server started on {server.sockets[0].getsockname()}")
+        logger.info(f"ShadowSocks Server started on {server.sockets[0].getsockname()}")
         async with server:
             await server.serve_forever()
 
     @staticmethod
-    async def connect(host: str, port: int) -> Connection:
+    async def connect(host: str, port: int) -> Tuple[asyncio.StreamReader, asyncio.StreamWriter]:
         reader, writer = await asyncio.open_connection(host, port)
-        connection = Connection(reader, writer)
-        return connection
+        return reader, writer
+
+if __name__ == '__main__':
+    logger.init_logger()
+
+    s = TcpServer(8888)
+    loop = asyncio.get_event_loop()
+    loop.create_task(s.run_server())
+    loop.run_forever()
+
 
